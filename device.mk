4c0710f2d3d54f31ecddf391c3cd433aa0b5b7c3
Soft-Bullet <40681532+Soft-Bullet@users.noreply.github.com>
2019-06-23 20:52:30 +0545

    a5ultexx: media fix

--- device.mk
+++ device.mk
@@ -203,7 +203,7 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     lights.msm8916
 
-# Media configurations
+# XML Media configurations
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
