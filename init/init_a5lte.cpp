d503a373db762d4f3aba529d8a9e7ccf36bb09fa
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-12-07 23:35:51 +0530

    a5ultexx: init: Rework vendor init extension and fix fingerprint
     * Cleaned up code, override ro.system and ro.vendor props
       (inspired from https://github.com/nicknitewolf/device_wingtech_wt88047/commit/94a8c39a33c89e40949d6e350187c478714a935d)
    
     * This also extends commit https://github.com/ArrowOS/android_frameworks_base/commit/8c56e7baada12133ed7f6f50433a918bfb5b193c which checks
       for ro.system.build.fingerprint instead of ro.build.fingerprint, otherwise the
       fingerprint check fails and "device has internal problem" message is displayed
       after every boot.

--- init/init_a5lte.cpp
+++ init/init_a5lte.cpp
@@ -73,13 +73,13 @@ void vendor_load_properties()
 
     if (bootloader.find("A500FU") == 0) {
         /* SM-A500FU */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultexx/a5ulte:6.0.1/MMB29M/A500FUXXU1CRH1:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultexx/a5ulte:6.0.1/MMB29M/A500FUXXU1CRH1:user/release-keys");
         property_override("ro.build.description", "a5ultexx-user 6.0.1 MMB29M A500FUXXU1CRH1 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500FU");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5ulte");
     } else if (bootloader.find("A500F") == 0) {
         /* SM-A500F */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ltexx/a5lte:6.0.1/MMB29M/A500FXXU1CRH2:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ltexx/a5lte:6.0.1/MMB29M/A500FXXU1CRH2:user/release-keys");
         property_override("ro.build.description", "a5ltexx-user 6.0.1 MMB29M A500FXXU1CRH2 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500F");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5lte");
@@ -87,7 +87,7 @@ void vendor_load_properties()
         init_dsds();
     } else if (bootloader.find("A500G") == 0) {
         /* SM-A500G */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ltedd/a5lte:6.0.1/MMB29M/A500GXXS1CRJ1:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ltedd/a5lte:6.0.1/MMB29M/A500GXXS1CRJ1:user/release-keys");
         property_override("ro.build.description", "a5ltedd-user 6.0.1 MMB29M A500GXXS1CRJ1 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500G");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5lte");
@@ -95,7 +95,7 @@ void vendor_load_properties()
         init_dsds();
     } else if (bootloader.find("A500M") == 0) {
         /* SM-A500M */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5lteub/a5lte:6.0.1/MMB29M/A500MUBS1CRI1:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5lteub/a5lte:6.0.1/MMB29M/A500MUBS1CRI1:user/release-keys");
         property_override("ro.build.description", "a5lteub-user 6.0.1 MMB29M A500MUBS1CRI1 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500M");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5lte");
@@ -103,13 +103,13 @@ void vendor_load_properties()
         init_dsds();
     } else if (bootloader.find("A500L") == 0) {
         /* SM-A500L */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultelgt/a5ultelgt:6.0.1/MMB29M/A500LKLU1CQC1:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultelgt/a5ultelgt:6.0.1/MMB29M/A500LKLU1CQC1:user/release-keys");
         property_override("ro.build.description", "a5ultelgt-user 6.0.1 MMB29M A500LKLU1CQC1 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500L");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5ulte");
     } else if (bootloader.find("A500H") == 0) {
         /* SM-A500H */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a53gxx/a53g:6.0.1/MMB29M/A500HXXS1CQC5:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a53gxx/a53g:6.0.1/MMB29M/A500HXXS1CQC5:user/release-keys");
         property_override("ro.build.description", "a53gxx-user 6.0.1 MMB29M A500HXXS1CQC5 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500H");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a53g");
@@ -117,37 +117,37 @@ void vendor_load_properties()
         init_dsds();
     } else if (bootloader.find("A500Y") == 0) {
         /* SM-A500Y */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultedv/a5ulte:6.0.1/MMB29M/A500YDVS1CRJ1:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultedv/a5ulte:6.0.1/MMB29M/A500YDVS1CRJ1:user/release-keys");
         property_override("ro.build.description", "a5ultedv-user 6.0.1 MMB29M A500YDVS1CRJ1 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500Y");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5ulte");
     } else if (bootloader.find("A500F1") == 0) {
         /* SM-A500F1 */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultekx/a5ulteskt:6.0.1/MMB29M/A500F1U1CQB1:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultekx/a5ulteskt:6.0.1/MMB29M/A500F1U1CQB1:user/release-keys");
         property_override("ro.build.description", "a5ultekx-user 6.0.1 MMB29M A500F1U1CQB1 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500F1");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5ulteskt");
     } else if (bootloader.find("A500S") == 0) {
         /* SM-A500S */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ulteskt/a5ulteskt:6.0.1/MMB29M/A500SKSU1CQB1:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ulteskt/a5ulteskt:6.0.1/MMB29M/A500SKSU1CQB1:user/release-keys");
         property_override("ro.build.description", "a5ulteskt-user 6.0.1 MMB29M A500SKSU1CQB1 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500S");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5ulteskt");
     } else if (bootloader.find("A500K") == 0) {
         /* SM-A500K */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultektt/a5ultektt:6.0.1/MMB29M/A500KKTU1CQB1:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultektt/a5ultektt:6.0.1/MMB29M/A500KKTU1CQB1:user/release-keys");
         property_override("ro.build.description", "a5ultektt-user 6.0.1 MMB29M A500KKTU1CQB1 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500K");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5ultektt");
     } else if (bootloader.find("A500W") == 0) {
         /* SM-A500W */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultebmc/a5ultebmc:6.0.1/MMB29M/A500WVLS1BPK2:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ultebmc/a5ultebmc:6.0.1/MMB29M/A500WVLS1BPK2:user/release-keys");
         property_override("ro.build.description", "a5ultebmc-user 6.0.1 MMB29M A500WVLS1BPK2 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500W");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5ultebmc");
     } else if (bootloader.find("A500YZ") == 0) {
         /* SM-A500YZ */
-        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ltezt/a5ltezt:5.0.2/LRX22G/A500YZZTU1BOK2:user/release-keys");
+        property_override_dual("ro.system.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/a5ltezt/a5ltezt:5.0.2/LRX22G/A500YZZTU1BOK2:user/release-keys");
         property_override("ro.build.description", "a5ltezt-user 5.0.2 LRX22G A500YZZTU1BOK2 release-keys");
         property_override_dual("ro.product.model", "ro.product.vendor.model", "SM-A500YZ");
         property_override_dual("ro.product.device", "ro.product.vendor.device", "a5ltezt");
